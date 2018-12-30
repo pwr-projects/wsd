ed604f6aa64c5c59b83d717293af6fc660469fb9
Mateusz Gawel <mrbalu9@gmail.com>
2018-12-31 00:07:17 +0100

    docker built based on nlptools, wcrft wrapper beginnings

---
+++ project/wrappers/wcrft.py
@@ -0,0 +1,5 @@
+def tag():
+	# touch in.txt
+	# echo "mam dziesięć złotych" > in.txt
+	# wcrft nkjp_e2.ini -d /usr/local/lib/python2.7/dist-packages/wcrft-1.0.0-py2.7.egg/wcrft/model/model_nkjp10_wcrft_e2/ -i txt in.txt -o iob-chan
+	print('tag')
